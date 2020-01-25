b83019c25ce3a27d4dcb18ab1eb2151e05c50fe1
David Van Fleet <dvfleet413@gmail.com>
2020-01-25 05:03:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,5 @@
 # code here!
+require 'pry'
 class School
   attr_accessor :roster
   attr_reader :name
@@ -8,6 +9,7 @@ class School
   end
   
   def add_student(name, grade)
+    binding.pry
     self.roster.grade = [] if !self.roster.grade
     self.roster.grade << name
   end 
